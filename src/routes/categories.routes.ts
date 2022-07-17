f1495e6ad2a94db2c984c6f09a4c519cfa16a6f8
Daniel Borges <danprog91@gmail.com>
2022-07-16 22:40:56 -0300

    Validando o cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoriesALreadyExists = categoriesRepository.findByName(name);
+
+  if (categoriesALreadyExists) {
+    return response.status(400).json({ error: 'Category Already Exists!' });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
